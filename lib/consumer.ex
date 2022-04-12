1897d95503b180f3d224b6f7eb68fea6ab716ee1
Wendel <wendelljuliao@hotmail.com>
2022-04-12 13:27:22 -0300

    5 comandos e meio feitos

--- lib/consumer.ex
+++ lib/consumer.ex
@@ -9,14 +9,23 @@ defmodule Botdiscord.Consumer do
 
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
     cond do
-      msg.content == "!ping" -> Api.create_message(msg.channel_id, "pong")
-      msg.content == "!yeye" -> Api.create_message(msg.channel_id, "glu glu :grin:")
-      
-      String.starts_with?(msg.content, "!ppt ") -> handlePPTCommand(msg)
-      msg.content == "!ppt" -> Api.create_message(msg.channel_id, "Comando para jogar pedra, papel ou tesoura\nUse **!ppt <elemento>**, onde _<elemento>_ deve ser _pedra_, _papel_, ou _tesoura_.")
-      
       String.starts_with?(msg.content, "!tempo ") -> handleWeather(msg)
       msg.content == "!tempo" -> Api.create_message(msg.channel_id, "Use **!tempo** <nome-da-cidade>")
+
+      String.starts_with?(msg.content, "!news ") -> handleNews(msg)
+      msg.content == "!news" -> Api.create_message(msg.channel_id, "Use !news <categoria>, onde categoria deve ser business, sports, politics, technology, startup")
+
+      String.starts_with?(msg.content, "!valorant ") -> handleValorant(msg)
+      msg.content == "!valorant" -> Api.create_message(msg.channel_id, "Use !valorant <personagem>, onde personagem deve ser o nome de um agente do jogo (ex: Killjoy, Reyna, etc...)")
+
+      String.starts_with?(msg.content, "!covid ") -> handleCovid(msg)
+      msg.content == "!covid" -> Api.create_message(msg.channel_id, "Use !covid <país>, onde país deve ser o nome do país em inglês (ex: Brazil, France, Portugal, etc...)")
+
+      String.starts_with?(msg.content, "!coffee") -> handleCoffee(msg)
+      
+      String.starts_with?(msg.content, "!pokemon ") -> handlePokemon(msg)
+      msg.content == "!pokemon" -> Api.create_message(msg.channel_id, "Use !pokemon <nome>, onde nome deve ser o nome de um pokemon (ex: Ditto, Charmander, Bulbasaur, etc...)")
+
 
 
       String.starts_with?(msg.content, "!") -> Api.create_message(msg.channel_id, "Comando inválido, tente novamente!")
@@ -29,6 +38,73 @@ defmodule Botdiscord.Consumer do
     :noop
   end
 
+  defp handlePokemon(msg) do
+    aux = String.split(msg.content, " ", parts: 2)
+    nome = String.downcase(Enum.fetch!(aux, 1))
+
+    resp = HTTPoison.get!("https://pokeapi.co/api/v2/pokemon-form/#{nome}")
+    
+    {isOk, map} = Poison.decode(resp.body)
+
+    case isOk != :error do
+      true -> 
+        pokemon = map["pokemon"]
+        Api.create_message(msg.channel_id, "Nome: #{pokemon["name"]}.\n #{map["sprites"]["front_default"]}")
+        
+      _ -> 
+        Api.create_message(msg.channel_id, "O nome #{nome} não foi encontrada. Tente novamente!")
+    end
+
+  end
+
+  defp handleCoffee(msg) do
+    resp = HTTPoison.get!("https://coffee.alexflipnote.dev/random.json")
+
+    {:ok, map} = Poison.decode(resp.body)
+
+    Api.create_message(msg.channel_id, map["file"])
+
+  end
+
+  defp handleCovid(msg) do
+    aux = String.split(msg.content, " ", parts: 2)
+    pais = String.capitalize(Enum.fetch!(aux, 1))
+
+    resp = HTTPoison.get!("https://covid-api.mmediagroup.fr/v1/cases?country=#{pais}")
+
+    {:ok, map} = Poison.decode(resp.body)
+
+    case map["All"] != nil do
+      true ->
+        Api.create_message(msg.channel_id, "Confirmados: #{map["All"]["confirmed"]} | Mortes: #{map["All"]["deaths"]}")
+      _ -> 
+        Api.create_message(msg.channel_id, "O país #{pais} não foi encontrado. Tente novamente!")
+
+    end
+  end
+
+  defp handleValorant(msg) do
+    :noop
+  end
+
+  defp handleNews(msg) do
+    aux = String.split(msg.content)
+    categoria = Enum.fetch!(aux, 1)
+    
+    resp = HTTPoison.get!("https://inshortsapi.vercel.app/news?category=#{categoria}")
+    
+    {:ok, map} = Poison.decode(resp.body)
+    
+    case map["success"] do
+      true -> 
+        noticia = Enum.fetch!(map["data"], 0)
+        Api.create_message(msg.channel_id, "Author #{noticia["author"]}: #{noticia["content"]}")
+      _ -> 
+        Api.create_message(msg.channel_id, "A categoria #{categoria} não foi encontrada. Tente novamente!")
+    end
+
+  end
+
   defp handleWeather(msg) do
     aux = String.split(msg.content, " ", parts: 2)
     cidade = Enum.fetch!(aux, 1)
@@ -38,7 +114,6 @@ defmodule Botdiscord.Consumer do
     {:ok, map} = Poison.decode(resp.body)
 
     case map["cod"] do
-
       200 -> 
         temp = map["main"]["temp"]
         Api.create_message(msg.channel_id, "A temperatura da cidade #{cidade} é de #{temp} °C")
@@ -48,9 +123,6 @@ defmodule Botdiscord.Consumer do
 
 
     end
-
-    
-
   end
 
   defp handlePPTCommand(msg) do
